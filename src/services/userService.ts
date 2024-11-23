2b1bc6b7d4af705c32b09fd88f1532c9ddfce4ae
mez3n <mez3n1151@gmail.com>
2024-11-23 23:55:55 +0200

    small bug fix

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,13 +1,17 @@
 import * as userRepository from '../repositories/userRepository';
 import { AppError, catchAsync } from '../utility';
-export const AddToBlocked = async (blockerId, blockedId) => {
+
+export const AddToBlocked = async (blockerId: number, blockedId: number) => {
   return userRepository.AddUserToBlocked(blockerId, blockedId);
 };
 
-export const RemoveFromBlocked = async (blockerId, blockedId) => {
+export const RemoveFromBlocked = async (
+  blockerId: number,
+  blockedId: number
+) => {
   return userRepository.RemoveUserFromBlocked(blockerId, blockedId);
 };
 
-export const GetUserBlockedList = async (blockerId) => {
+export const GetUserBlockedList = async (blockerId: number) => {
   return userRepository.GetUserBlockedList(blockerId);
 };
