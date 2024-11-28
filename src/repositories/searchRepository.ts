5c470418b5ecd3d385eec9e7f7e5c682c17b8530
mez3n <mez3n1151@gmail.com>
2024-11-28 20:23:20 +0200

    updated search and block

--- src/repositories/searchRepository.ts
+++ src/repositories/searchRepository.ts
@@ -4,20 +4,11 @@ const prisma = new PrismaClient();
 
 export const findProfilesByUsername = async (username: string) => {
   return prisma.$queryRaw`
-        SELECT username,
-               password,
-               email,
+        SELECT id,
+               username               AS name,
                photo,
-               phone,
-               bio,
-               screen_name              AS screenName,
-               auto_download_size_limit AS autoDownloadSizeLimit,
-               max_limit_file_size      AS maxLimitFileSize,
-               profile_pic_visibility   AS profilePicVisibility,
-               story_visibility         AS storyVisibility,
-               read_receipts_enabled    AS readReceiptsEnabled,
-               last_seen                AS lastSeen,
-               group_add_permission     AS groupAddPermission
+               screen_name            AS screenName,
+               profile_pic_visibility AS profilePicVisibility
         FROM users
         WHERE LOWER(username) LIKE LOWER(${`%${username}%`});
     `;
@@ -25,7 +16,8 @@ export const findProfilesByUsername = async (username: string) => {
 
 export const findGroupByGroupName = async (groupName: string) => {
   return prisma.$queryRaw`
-        SELECT *
+        SELECT name,
+               groups.id
         FROM communities,
              groups
         WHERE communities.privacy = true
@@ -38,12 +30,13 @@ export const findGroupByGroupName = async (groupName: string) => {
 
 export const findChannelByChannelName = async (channelName: string) => {
   return prisma.$queryRaw`
-        SELECT *
+        SELECT name,
+               channels.id
         FROM communities,
              channels
         WHERE communities.privacy = true
           --TODO uncomment when db is updated
-        --  AND communities.active = true
+          --  AND communities.active = true
           AND communities.id = channels.community_id
           AND LOWER(communities.name) LIKE LOWER(${'%' + channelName + '%'})
     `;
--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,43 +1,55 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import prisma from '../prisma/client';
 
-import { Users,Admins } from '@prisma/client';
+import { Users, Admins } from '@prisma/client';
 import { OAuthUser } from './repositoriesTypes/authTypes';
-import { Social } from "@prisma/client";
+import { Social } from '@prisma/client';
 import { AppError } from '../utility';
 
 // should take Omit<Users, 'id'> as the parameter type
-export const createUser = async (user:any): Promise<Users> => {
+export const createUser = async (user: any): Promise<Users> => {
   return prisma.users.create({ data: user });
 };
 
-export const findUserByEmail = async (email: string): Promise<Users | null|Admins> => {
-  const normal_user=await prisma.users.findUnique({ where: { email } });
-  if(normal_user){
+export const findUserByEmail = async (
+  email: string
+): Promise<Users | null | Admins> => {
+  const normal_user = await prisma.users.findUnique({ where: { email } });
+  if (normal_user) {
     return normal_user;
   }
-  const admin_user=await prisma.admins.findUnique({ where: { email } ,include: {
-    bannedUsers: true,
-  },});
+  const admin_user = await prisma.admins.findUnique({
+    where: { email },
+    include: {
+      bannedUsers: true,
+    },
+  });
   return admin_user;
-
 };
 
-export const findUserByUsername = async (username: string): Promise<Users | null> => {
+export const findUserByUsername = async (
+  username: string
+): Promise<Users | null> => {
   return await prisma.users.findUnique({ where: { username } });
-}
+};
 
-export const findUserByProvider= async (providerId: string, providerType: Social): Promise<Users | null> => {
+export const findUserByProvider = async (
+  providerId: string,
+  providerType: Social
+): Promise<Users | null> => {
   return await prisma.users.findFirst({ where: { providerId, providerType } });
-}
+};
 
 export const findUserById = async (id: number): Promise<Users | null> => {
   return await prisma.users.findUnique({ where: { id } });
-}
+};
 
-export const updateUserById = async (id: number, data: Partial<Users>): Promise<Users> => {
+export const updateUserById = async (
+  id: number,
+  data: Partial<Users>
+): Promise<Users> => {
   return await prisma.users.update({ where: { id }, data });
-}
+};
 
 export const storeOAuthUser = async (user: OAuthUser): Promise<Users> => {
   const providerType = Social[user.provider as keyof typeof Social];
@@ -46,20 +58,18 @@ export const storeOAuthUser = async (user: OAuthUser): Promise<Users> => {
   }
   const newUser = await prisma.users.create({
     data: {
-      email: user.email as string || '',
+      email: (user.email as string) || '',
       username: user.userName,
       password: '',
       providerType,
       providerId: user.providerId,
       photo: user.picture,
       IsEmailVerified: user.email_verified,
-      public_key:""
-
+      public_key: '',
     },
   });
-  console.log("USER CREATED")
+  console.log('USER CREATED');
   return newUser;
-
 };
 export const AddUserToBlocked = async (
   blockerId: number,
@@ -93,7 +103,14 @@ export const GetUserBlockedList = async (blockerId: number) => {
       blockerId: blockerId,
     },
     include: {
-      blockedUser: true,
+      blockedUser: {
+        select: {
+          id: true,
+          username: true,
+          photo: true,
+          profilePicVisibility: true,
+        },
+      },
     },
   });
 };
