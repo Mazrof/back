67e4d7b00f3139044d56969653de52edbbc2b38a
Ahmed-El-sayed-Mahmoud <ahmed.elsayed@snowheap.com>
2024-12-09 10:40:55 +0200

    fix

--- src/middlewares/sessionHandler.ts
+++ src/middlewares/sessionHandler.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express';
 
 export const OAuthSessionHandler = (req: Request, res: Response) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  req.session.user = { id: (req.user as any).id, userType: 'user' }; // Store user in session
+  req.session.user = { id: (req.user as any).id, userType: 'user' ,user:req.user}; // Store user in session
   res.redirect(`${process.env.FRONTEND_URL}`);
 };
