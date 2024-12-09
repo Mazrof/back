2e581b36e0878fa76630538c76d51d6a18bff5a6
Ahmed-El-sayed-Mahmoud <ahmed.elsayed@snowheap.com>
2024-12-09 10:44:01 +0200

    small fix

--- src/__tests__/authControllerTests.ts
+++ src/__tests__/authControllerTests.ts
@@ -122,7 +122,7 @@ describe('Authentication Controller', () => {
       save: jest.fn(),
       touch: jest.fn(),
       resetMaxAge: jest.fn(),
-      user: { id: 1, userType: 'user' },
+      user: { id: 1, userType: 'user' ,user:null},
     };
 
     // Call the controller method
