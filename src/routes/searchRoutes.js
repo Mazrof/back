0a3e92f8824b05eadc34381c973512c1936471fd
abdomohamed96 <abdomohamed9192@gmail.com>
2024-12-19 20:24:13 +0200

    Remove js files

--- src/routes/searchRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var searchController = require('./../controllers/searchController');
-var router = express_1.default.Router();
-// TODO: add user authentication
-router.get('', searchController.generalSearch);
-// Export the router
-exports.default = router;
