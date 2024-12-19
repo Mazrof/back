d6566de6f32d126c3a2fb647447e45799ac60512
mez3n <mez3n1151@gmail.com>
2024-12-19 16:11:45 +0200

    updated general search response

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -1,13 +1,38 @@
 import * as searchRepository from '../repositories/searchRepository';
+import { getFileFromFirebase } from '../third_party_services';
 
 export const getProfileByUsername = async (username: string) => {
-  return await searchRepository.findProfilesByUsername(username);
+  return searchRepository.findProfilesByUsername(username);
 };
 
 export const getGroupByGroupName = async (groupName: string) => {
-  return await searchRepository.findGroupByGroupName(groupName);
+  const groups: any[] = await searchRepository.findGroupByGroupName(groupName);
+  return await Promise.all(
+    groups.map(async (group) => ({
+      id: group.id,
+      groupSize: group.groupsize,
+      community: {
+        name: group.name,
+        privacy: group.privacy,
+        imageURL: await getFileFromFirebase(group.imageurl),
+      },
+    }))
+  );
 };
 
-export const getChannelByChannelName = async (groupName: string) => {
-  return await searchRepository.findChannelByChannelName(groupName);
+export const getChannelByChannelName = async (channelName: string) => {
+  const channels: any[] =
+    await searchRepository.findChannelByChannelName(channelName);
+  return await Promise.all(
+    channels.map(async (channel) => ({
+      id: channel.id,
+      invitationLink: channel.invitationlink,
+      canAddComments: channel.canaddcomments,
+      community: {
+        name: channel.name,
+        privacy: channel.privacy,
+        imageURL: await getFileFromFirebase(channel.imageurl),
+      },
+    }))
+  );
 };
