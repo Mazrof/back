4947623e1378141ad6db0b533419a5d16036e4d3
mez3n <mez3n1151@gmail.com>
2024-12-19 16:14:39 +0200

    updated general search response with a minor fix

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -1,13 +1,33 @@
 import * as searchRepository from '../repositories/searchRepository';
 import { getFileFromFirebase } from '../third_party_services';
 
+type Group = {
+  id: string;
+  groupsize: number;
+  name: string;
+  privacy: string;
+  imageurl: string;
+};
+
+type Channel = {
+  id: string;
+  invitationlink: string;
+  canaddcomments: boolean;
+  name: string;
+  privacy: string;
+  imageurl: string;
+};
+
 export const getProfileByUsername = async (username: string) => {
   return searchRepository.findProfilesByUsername(username);
 };
 
 export const getGroupByGroupName = async (groupName: string) => {
-  const groups: any[] = await searchRepository.findGroupByGroupName(groupName);
-  return await Promise.all(
+  const groups = (await searchRepository.findGroupByGroupName(groupName)) as
+    | Group[]
+    | null;
+
+  return Promise.all(
     groups.map(async (group) => ({
       id: group.id,
       groupSize: group.groupsize,
@@ -21,9 +41,11 @@ export const getGroupByGroupName = async (groupName: string) => {
 };
 
 export const getChannelByChannelName = async (channelName: string) => {
-  const channels: any[] =
-    await searchRepository.findChannelByChannelName(channelName);
-  return await Promise.all(
+  const channels = (await searchRepository.findChannelByChannelName(
+    channelName
+  )) as Channel[] | null;
+
+  return Promise.all(
     channels.map(async (channel) => ({
       id: channel.id,
       invitationLink: channel.invitationlink,
